3e6f3d5874a06bb27998f8c0b5ae4619bfac7b96
stuartErnstsen <stuarternstsen@gmail.com>
2020-12-27 20:31:19 -0700

    Completed functionality for get/put/delete/post on social mountain lab, now working on search functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post'
 
 import './App.css';
 
@@ -13,25 +15,33 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+      .then(results => {
+        this.setState({ posts: results.data })
+      })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then(result => {
+        this.setState({ posts: result.data })
+      })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then(results => this.setState({ posts: results.data }))
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text })
+      .then(results => this.setState({ posts: results.data }))
   }
 
   render() {
@@ -43,8 +53,8 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          {posts.map(post => <Post key={post.id} id={post.id} text={post.text} date={post.date} updatePostFn={this.updatePost} deletePostFn={this.deletePost}></Post>)}
         </section>
       </div>
     );
